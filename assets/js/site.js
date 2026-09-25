9b4851cf21f992053bed85ac779b2e71b78cf2f4
jhlocal <jhlocal@ppn.local>
2026-09-25 22:29:47 +0100

    mirror Willington

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,1 @@
-const button=document.querySelector('.menu-toggle');const nav=document.querySelector('.nav');if(button&&nav){button.addEventListener('click',()=>{const open=nav.classList.toggle('open');button.setAttribute('aria-expanded',String(open));});}
+document.querySelector('.menu-toggle')?.addEventListener('click',()=>document.querySelector('.nav').classList.toggle('open'));
